9f5a52570007f4cb4908beec3ae926d8ed98c33d
kelysot <kelysot@gmail.com>
2022-03-02 14:24:34 +0200

    try to make the project work on mac and windows.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,11 @@
 
 const User = require('../models/user_model')
-const bcrypt = require('bcrypt')
-const {use} = require('../routes')
+var bcrypt = require('bcryptjs');
+const { use } = require('../routes')
 const jwt = require('jsonwebtoken')
 
-const sendError = (res, code, msg)=>{
-     return res.status(code).send({
+const sendError = (res, code, msg) => {
+    return res.status(code).send({
         'status': 'fail',
         'error': msg
     })
@@ -17,27 +17,27 @@ const register = async (req, res) => {
     const email = req.body.email
     const password = req.body.password
 
-    if(email == null || password == null){
+    if (email == null || password == null) {
         return sendError(res, 400, "wrong email or password")
     }
 
-    try{
-        const exist = await User.findOne({'email' : email})
-        if (exist != null){
+    try {
+        const exist = await User.findOne({ 'email': email })
+        if (exist != null) {
             return res.status(400).send({
                 'status': 'fail',
-                'error':'user exist'
+                'error': 'user exist'
             })
-        } 
+        }
         const salt = await bcrypt.genSalt(10)
-        const hashPwd = await bcrypt.hash(password,salt)
+        const hashPwd = await bcrypt.hash(password, salt)
         const user = User({
-            'email' : email,
-            'password' : hashPwd
-        })  
+            'email': email,
+            'password': hashPwd
+        })
         newUser = await user.save();
         res.status(200).send(newUser);
-    }catch(err){
+    } catch (err) {
         res.status(400).send({
             'status': 'fail',
             'error': err.message
@@ -49,25 +49,25 @@ const login = async (req, res) => {
 
     const email = req.body.email
     const password = req.body.password
-    if(email == null || password == null) return sendError(res, 400, "wrong email or password")
-    
-    try{
+    if (email == null || password == null) return sendError(res, 400, "wrong email or password")
 
-        const user = await User.findOne({'email' : email})
-        if(user == null) sendError(res, 400, "wrong email or password")
-        
+    try {
+
+        const user = await User.findOne({ 'email': email })
+        if (user == null) sendError(res, 400, "wrong email or password")
+
         const match = await bcrypt.compare(password, user.password)
         if (!match) return sendError(res, 400, "wrong email or password")
-       
+
 
         const accessToken = await jwt.sign(
-            {'id':user._id},
+            { 'id': user._id },
             process.env.ACCESS_TOKEN_SECRET,
-            {expiresIn: process.env.JWT_TOKEN_EXPIRATION}
+            { expiresIn: process.env.JWT_TOKEN_EXPIRATION }
         )
-        res.status(200).send({'accessToken' : accessToken});
+        res.status(200).send({ 'accessToken': accessToken });
 
-    }catch(err){
+    } catch (err) {
         return sendError(res, 400, err.message)
     }
 }
@@ -79,7 +79,7 @@ const logout = async (req, res) => {
     })
 }
 
-module.exports ={
+module.exports = {
     login,
     register,
     logout
