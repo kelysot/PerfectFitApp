bc58262d044980dd9073baed2bcb87f3815d1171
Yarin Matmoni <yarinmatmoni@gmail.com>
2022-03-04 12:15:53 +0200

    swagger post - change according the schema

--- routes/post_routes.js
+++ routes/post_routes.js
@@ -15,22 +15,108 @@ const authenticate = require('../common/auth_middleware')
 /**
 * @swagger
 * components:
+*   securitySchemes:
+*       bearerAuth:
+*           type: http
+*           scheme: bearer
+*           bearerFormat: JWT
+*/
+
+/**
+* @swagger
+* components:
 *   schemas:
 *     Post:
 *       type: object
 *       required:
-*         - message
-*         - sender
+*         -profileId
+*         -productName
+*         -sku
+*         -size
+*         -company
+*         -price
+*         -color
+*         -categoryId
+*         -subCategoryId
+*         -description
+*         -date
+*         -link
+*         -sizeAdjustment
+*         -rating
+*         -picturesUrl
+*         -likes
+*         -comments
 *       properties:
-*         message:
-*           type: string
-*           description: The post text 
-*         sender:
-*           type: string
-*           description: The user who send the post id
+*        profileId:
+*           type: String
+*           description: The profile ID
+*        productName:
+*           type: String
+*           description: The name of the product
+*        sku:
+*           type: String
+*           description: The serial number of the product
+*        size:
+*           type: String
+*           description: The size of the product
+*        company:
+*           type: String
+*           description: The company of the product
+*        price:
+*           type: String
+*           description: The price of the product
+*        color:
+*           type: String
+*           description: The color of the product
+*        categoryId:
+*           type: String
+*           description: The main category ID of the product
+*        subCategoryId:
+*           type: String
+*           description: The secondary category ID of the product
+*        description:
+*           type: String
+*           description: The descriptor of the product
+*        date:
+*           type: String
+*           description: The date the post posted
+*        link:
+*           type: String
+*           description: Link for the product
+*        sizeAdjustment:
+*           type: String
+*           description: How much the product fits the body structure of the customer
+*        rating:
+*           type: String
+*           description: Product rating in general
+*        picturesUrl:
+*           type: String
+*           description: An array of links to the images that displayed in the post
+*        likes:
+*           type: mongoose.Schema.Types.ObjectId
+*           description: An array of profiles ID who liked the post
+*        comments:
+*           type: mongoose.Schema.Types.ObjectId
+*           description: An array of profiles ID who comment on the post
 *       example:
-*         message: 'this is swagger test message'
-*         sender: '123456'
+*         profileId: 'Yarin'
+*         productName: 'shirt'
+*         sku: 'KS123456'
+*         size: 'Large' 
+*         company: 'Renuar'
+*         price: '125.00'
+*         color: 'blue'
+*         categoryId: 'Shirt'
+*         subCategoryId: 'T-shirt'
+*         description: 'Nice shirt, very comfortable'
+*         date: '22/03/2022'
+*         link: 'https://www.renuar.co.il/he/men/sale/hvlcvt-ti-qcrvt-3-b-99-90-w-h/422895519.html'
+*         sizeAdjustment: 'The shirt rests well on the body'
+*         rating: 'The fabric of the shirt is very comfortable and the color is beautiful'
+*         picturesUrl: ["https://www.renuar.co.il/media/catalog/product/cache/2/image/9df78eab33525d08d6e5fb8d27136e95/4/2/422895519-1.jpg",
+*                       "https://www.renuar.co.il/media/catalog/product/cache/2/image/9df78eab33525d08d6e5fb8d27136e95/4/2/422895519-2.jpg"]
+*         likes: ["E215616547418","YE4564651236"]
+*         comments: ["E215616547418","AE45646546"]
 */
 
 /**
