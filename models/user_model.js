06300d1532ef0017ab16edcf3604898bfc726789
kelysot <kelysot@gmail.com>
2022-03-30 19:45:18 +0300

    Add isConnected to the user which already change when user login\register\logout.

--- models/user_model.js
+++ models/user_model.js
@@ -13,6 +13,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    isConnected: {
+        type: String,
+        required: true
+    },
     profilesId: [{
         type: String,
         ref: 'Profile'
