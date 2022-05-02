d60dba423d78d2dbcac29993cb070a84d2db01fd
kelysot <kelysot@gmail.com>
2022-05-02 11:26:30 +0300

    Get and upload images works with android.

--- routes/images.js
+++ routes/images.js
@@ -21,38 +21,25 @@ var storage = multer.diskStorage({
 
 var image = multer({ storage: storage })
 
+router.post("/upload", image.single("upload"), async (req, res) => {
+    if (req.file === undefined) return res.status(400).send;
+    let imgUrl = req.file.path;
+    res.status(200).send(imgUrl);
+});
 
-router.post('/upload', image.single("upload"), async (req, res) => {
+router.get("/image/:imageName", async (req, res) => {
 
-    if (req.file === undefined) return res.status(404).send()
+    let imagePath = req.params.imageName
+    if (imagePath == null) res.status(404).send()
 
-    let path = `/images/${req.file.filename}`
-    res.status(200).json({ message: 'Image Uploaded Successfully !', path: path })
-})
-
-// router.get('/images/:imagename', (req, res) => {
-
-//     let imagename = req.params.imagename
-//     let imagepath = __dirname + "/images/" + imagename
-//     let image = fs.readFileSync(imagepath)
-//     let mime = fileType(image).mime
-
-//     res.writeHead(200, { 'Content-Type': mime })
-//     res.end(image, 'binary')
-// })
-
-router.get("/images/:imagename", async (req, res) => {
-    let ans = req.params.imagename.toString()
-    if (ans === "null") res.status(404).send()
-
-    fs.readFile(req.params.imagename, function (err, data) {
+    fs.readFile(imagePath, function (err, data) {
         try {
-            res.writeHead(200, { 'Content-Type': 'image/jpeg' })
-            res.end(data)
+            res.writeHead(200, { 'Content-Type': 'image/png' });
+            res.end(data); // Send the file data to the browser.  
         } catch (err) {
-            err.status(400).send()
+            res.status(400).send();
         }
-    })
-})
+    });
+});
 
 module.exports = router
